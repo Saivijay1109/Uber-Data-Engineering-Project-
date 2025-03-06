2321f96fcd3b662e80db307201a291d29bc66b4f
Saivijay Panuganti <93557911+Saivijay1109@users.noreply.github.com>
2025-03-06 15:36:43 -0500

    Update analytics_query.sql

--- analytics_query.sql
+++ analytics_query.sql
@@ -21,12 +21,12 @@ f.improvement_surcharge,
 f.total_amount
 FROM 
 
-`my_test_project_vijay.uber_data_engineering_yt.fact_table` f
-JOIN `my_test_project_vijay.uber_data_engineering_yt.datetime_dim` d  ON f.datetime_id=d.datetime_id
-JOIN `my_test_project_vijay.uber_data_engineering_yt.passenger_count_dim` p  ON p.passenger_count_id=f.passenger_count_id  
-JOIN `my_test_project_vijay.uber_data_engineering_yt.trip_distance_dim` t  ON t.trip_distance_id=f.trip_distance_id  
-JOIN `my_test_project_vijay.uber_data_engineering_yt.rate_code_dim` r ON r.rate_code_id=f.rate_code_id  
-JOIN `my_test_project_vijay.uber_data_engineering_yt.pickup_location_dim` pick ON pick.pickup_location_id=f.pickup_location_id
-JOIN `my_test_project_vijay.uber_data_engineering_yt.dropoff_location_dim` drop ON drop.dropoff_location_id=f.dropoff_location_id
-JOIN `my_test_project_vijay.uber_data_engineering_yt.payment_type_dim` pay ON pay.payment_type_id=f.payment_type_id)
+`my_test_project_vijay.uber_data_engineering_yt.fact_table` AS f
+JOIN `my_test_project_vijay.uber_data_engineering_yt.datetime_dim` AS d  ON f.datetime_id=d.datetime_id
+JOIN `my_test_project_vijay.uber_data_engineering_yt.passenger_count_dim` AS p  ON p.passenger_count_id=f.passenger_count_id  
+JOIN `my_test_project_vijay.uber_data_engineering_yt.trip_distance_dim` AS t  ON t.trip_distance_id=f.trip_distance_id  
+JOIN `my_test_project_vijay.uber_data_engineering_yt.rate_code_dim` AS r ON r.rate_code_id=f.rate_code_id  
+JOIN `my_test_project_vijay.uber_data_engineering_yt.pickup_location_dim` AS pick ON pick.pickup_location_id=f.pickup_location_id
+JOIN `my_test_project_vijay.uber_data_engineering_yt.dropoff_location_dim` AS drop ON drop.dropoff_location_id=f.dropoff_location_id
+JOIN `my_test_project_vijay.uber_data_engineering_yt.payment_type_dim` AS pay ON pay.payment_type_id=f.payment_type_id)
 ;
